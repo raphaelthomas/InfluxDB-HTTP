9b978cb1fe1aa06800819ffc4585d126d8de8745
Raphael Seebacher <rse@open.ch>
2016-05-27 10:10:53 +0200

    Use MIT license

--- lib/InfluxDB/HTTP.pm
+++ lib/InfluxDB/HTTP.pm
@@ -222,7 +222,7 @@ Returns the internally used LWP::UserAgent instance for possible modifications
 
 =head1 AUTHOR
 
-Raphael Seebacher, C<< <raphael at seebachers.ch> >>
+Raphael Seebacher, C<< <raphael@seebachers.ch> >>
 
 =head1 BUGS
 
@@ -231,12 +231,26 @@ L<https://github.com/raphaelthomas/InfluxDB-HTTP/issues>.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2016 Raphael Seebacher.
+MIT License
 
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
+Copyright (c) 2016 Raphael Seebacher
 
-See L<http://dev.perl.org/licenses/> for more information.
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
 
 =cut
